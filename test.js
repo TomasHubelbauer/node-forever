d7f95d0bb82f7119f1f038c7742f153cc1d571ba
Tomáš Hübelbauer <tomas@hubelbauer.net>
2022-04-10 20:29:33 +0200

    Remove void async wrapper and use TLA
    Top level await has landed and is now in recent versions of Node.

--- test.js
+++ test.js
@@ -1,25 +1,19 @@
 import forever from './index.js';
 import http from 'http';
 
-// Kill the process on an unhandled promise rejection and unhandled exception
-process.on('unhandledRejection', error => { throw error; });
-process.on('uncaughtException', error => { throw error; });
+// Make this process standalone and single-instance and propagate the standard I/O
+if (await forever()) {
+  return;
+}
 
-void async function () {
-  // Make this process standalone and single-instance and propagate the standard I/O
-  if (await forever()) {
-    return;
-  }
+// Wait a bit for the existing agent to die before attempting to bind the port
+// TODO: Do this in a smarter way - try to bind and wait and retry on failure
+await new Promise(resolve => setTimeout(resolve, 1000));
 
-  // Wait a bit for the existing agent to die before attempting to bind the port
-  // TODO: Do this in a smarter way - try to bind and wait and retry on failure
-  await new Promise(resolve => setTimeout(resolve, 1000));
-
-  const port = process.argv[2] || 1337;
-  http
-    .createServer((request, response) => {
-      response.end(request.url);
-    })
-    .listen(port, () => console.log(process.pid, 'http://localhost:' + port))
-    ;
-}()
+const port = process.argv[2] || 1337;
+http
+  .createServer((request, response) => {
+    response.end(request.url);
+  })
+  .listen(port, () => console.log(process.pid, 'http://localhost:' + port))
+  ;
